1784d2225f6cd018e55536fb5efcc5e395a85343
mihailsokolov90 <mihailsokolov90@gmail.com>
2021-11-10 17:19:48 +0300

    message form setup

--- Koterov Kostarev - PHP 7/Part 6 - Net Functions/message_form.php
+++ Koterov Kostarev - PHP 7/Part 6 - Net Functions/message_form.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html lang="ru">
-<head>
+<head class="h-100">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -10,8 +10,68 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <!-- END OF CSS SECTION ---------------------------------------------------------------------------------- -->
 </head>
-<body>
-<!-- JAVASCRIPT SECTION ----------------------------------------------------------------------------------------- -->
+<body class="h-100 d-flex ">
+<!-- HEADER SECTION ----------------------------------------------------------------------------------------- -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top sticky-top">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand" role="button">
+                <img src="img/logo.png" height="30">
+            </a>
+            <button role="button" class="navbar-toggler " data-bs-target="nav_main" data-bs-toggle="collapse"
+                    aria-expanded="false" aria-controls="#nav_main">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="nav_main">
+                <ul class="navbar-nav">
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- END OF HEADER SECTION ---------------------------------------------------------------------------------- -->
+
+<!-- MAIN SECTION ----------------------------------------------------------------------------------------- -->
+<main class="flex-shrink-0">
+
+</main>
+<!-- END OF MAIN SECTION ---------------------------------------------------------------------------------- -->
+
+<!-- FOOTER SECTION ----------------------------------------------------------------------------------------- -->
+<footer class="mt-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <img src="img/logo.png" alt="" class="img-fluid footer-brand">
+            </div>
+            <div class="col-md-6">
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item">
+                        <a href="#" class="h4">Main</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="h4">Company</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="h4">About</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="h4">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <address>
+                    Some address string <br/>
+                    St.Main Avenue, 31b <br>
+                    <abbr title="Phone">P:</abbr>8-343-987-23-12 <br>
+                </address>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- END OF FOOTER SECTION ---------------------------------------------------------------------------------- -->
 
 <!-- JAVASCRIPT SECTION ----------------------------------------------------------------------------------------- -->
 <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
