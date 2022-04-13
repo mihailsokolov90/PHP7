c61e0701bef9382dd252ffaa20d6a2f82bf30830
mihailsokolov90 <mihailsokolov90@gmail.com>
2022-04-13 17:26:43 +0300

    Доделал базовый UI

--- Koterov Kostarev - PHP 7/Part 1 - Base/index.php
+++ Koterov Kostarev - PHP 7/Part 1 - Base/index.php
@@ -8,18 +8,50 @@
     <title>PHP 7 - Part 1</title>
     <!-- CSS SECTION ----------------------------------------------------------------------------------------- -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <!-- END OF CSS SECTION ---------------------------------------------------------------------------------- -->
 </head>
-<body class="h-100 d-flex flex-row">
+<body class="h-100 d-flex flex-column">
 <!-- HEADER SECTION ----------------------------------------------------------------------------------------- -->
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-        <div class="container">
-            <a href="#" role="button" class="navbar-brand">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">
                 <img src="img/logo.png" alt="No Brand" class="img-fluid">
             </a>
-            <button role="button"></button>
+            <button role="button" class="navbar-toggler" aria-controls="header_nav_collapse" aria-expanded="false"
+               data-bs-target="#header_nav_collapse" data-bs-toggle="collapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="header_nav_collapse">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="fa fa-home"></span>
+                            <sapn>Main</sapn>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="fa fa-home"></span>
+                            <sapn>Links</sapn>
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" aria-expanded="false" aria-haspopup="true"
+                           data-bs-toggle="dropdown" id="dropdown-menu-items">
+                            <span class="fa"></span>
+                            Items
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown-menu-items">
+                            <li><a href="#" class="dropdown-item">Item one</a></li>
+                            <li><span class="dropdown-divider"></span></li>
+                            <li><a href="#" class="dropdown-item">Item two</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
