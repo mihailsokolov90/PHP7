b7dab8f26af0848a174c966d6479913dd77d0c53
mihailsokolov90 <mihailsokolov90@gmail.com>
2022-05-20 17:40:14 +0300

    Registration Test

---
+++ Koterov Kostarev - PHP 7/Part 1 - Base/js/registration.js
@@ -0,0 +1,24 @@
+/**
+ * @author Michael
+ * @brief Main
+ */
+
+class RegistrationCore
+{
+    constructor()
+    {
+
+    }
+
+    Main()
+    {
+
+    }
+}
+
+
+$(function ()
+{
+    const app = new RegistrationCore();
+    app.Main();
+});
---
+++ Koterov Kostarev - PHP 7/Part 1 - Base/registration.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration Form</title>
+    <!-- CSS ------------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/registration.css" type="text/css">
+    <!-- END OF CSS ------------------------------------------------------------------------------------------------>
+</head>
+<body>
+<!-- HEADER ------------------------------------------------------------------------------------------------------->
+<header>
+    <nav class="navbar navbar-expand-lg bg-light navbar-light sticky-top">
+        <a href="#" role="button" class="navbar-brand">
+            <img src="img/logo.png" alt="" class="navbar-brand-img">
+        </a>
+        <button role="button" class="navbar-toggler" >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="header_nav_collapse_content">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href=""></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<!-- END OF HEADER ------------------------------------------------------------------------------------------------>
+
+<!-- MAIN ------------------------------------------------------------------------------------------------------->
+<!-- END OF MAIN ------------------------------------------------------------------------------------------------>
+
+<!-- FOOTER ------------------------------------------------------------------------------------------------------->
+<!-- END OF FOOTER ------------------------------------------------------------------------------------------------>
+
+<!-- JS ------------------------------------------------------------------------------------------------------->
+<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript" src="js/"></script>
+<!-- END OF JS ------------------------------------------------------------------------------------------------>
+</body>
+</html>
