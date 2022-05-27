e33c69cc54f2c92c9e1920a24161a9bb091b1af6
mihailsokolov90 <mihailsokolov90@gmail.com>
2022-05-27 17:24:20 +0300

    Chapter 4 - External/css/main.css Chapter 4 - External/index.php

--- Zandstra - PHP 8/Chapter 4 - External/index.php
+++ Zandstra - PHP 8/Chapter 4 - External/index.php
@@ -20,21 +20,73 @@
             <a href="#" class="navbar-brand" role="button">
                 <img src="img/logo.png" alt="" class="navbar-brand-img">
             </a>
-            <button role="button" class="navbar-toggler" aria-expanded="false" aria-controls="header_nav_collapse_content" id="header_nav_toggler">
+            <button role="button" class="navbar-toggler" data-bs-target="#header_nav_collapse_content" data-bs-toggle="collapse"
+                    aria-expanded="false" aria-controls="header_nav_collapse_content" id="header_nav_toggler">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="navbar-collapse collapse" id="header_nav_collapse_content" aria-labelledby="header_nav_toggler"></div>
+            <div class="navbar-collapse collapse" id="header_nav_collapse_content" aria-labelledby="header_nav_toggler">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            <span class="fa "></span>
+                            <span>Main</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
 <!-- END OF HEADER ---------------------------------------------------------------------------------------------->
 <!-- MAIN ----------------------------------------------------------------------------------------------------->
 <main class="flex-shrink-0">
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="console"></div>
+            </div>
+        </div>
+    </div>
 </main>
 <!-- END OF MAIN ---------------------------------------------------------------------------------------------->
 <!-- FOOTER ----------------------------------------------------------------------------------------------------->
-<footer class="mt-auto"></footer>
+<footer class="mt-auto">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3">
+                <img src="img/logo.png" alt="" class="img-fluid">
+            </div>
+            <div class="col-6">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <h4>Main</h4>
+                        </div>
+                        <div class="col">
+                            <h4>Products</h4>
+                        </div>
+                        <div class="col">
+                            <h4>Contacts</h4>
+                        </div>
+                        <div class="col">
+                            <h4>Help</h4>
+                        </div>
+                        <div class="col">
+                            <h4>Authors</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <h5>Company Address</h5>
+                <address>
+                    Some address 144654 <br>Moscow, Russia <br>
+                    <abbr title="Phone">P:</abbr>8 495 322-23-23
+                </address>
+            </div>
+        </div>
+    </div>
+</footer>
 <!-- END OF FOOTER ---------------------------------------------------------------------------------------------->
 <!-- JS ----------------------------------------------------------------------------------------------------->
 <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
